869b9aff338c49434b0890521adc525e54608db1
Rdiariv <rdr1982@gmail.com>
2022-04-09 08:51:07 +0100

    segundo commit para la suma

--- src/evaluacion/EvalucionGit.java
+++ src/evaluacion/EvalucionGit.java
@@ -5,7 +5,11 @@ public class EvalucionGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola mundo"); 
+		System.out.println("Hola mundo");
+		
+		int a=5, b=7;
+		
+		System.out.println("La suma de "+a+" y "+b+" es "+(a+b));
 	}
 
 }
