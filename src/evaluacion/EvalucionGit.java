96b579966d205c8eac1281ab0e65ca19cc7f6468
Rdiariv <rdr1982@gmail.com>
2022-04-09 08:59:41 +0100

    Calcula factorial

--- src/evaluacion/EvalucionGit.java
+++ src/evaluacion/EvalucionGit.java
@@ -8,6 +8,7 @@ public class EvalucionGit {
 		System.out.println("Hola mundo");
 		
 		int a=5, b=7;
+		int factorial=1;
 		
 		System.out.println("La suma de "+a+" y "+b+" es "+(a+b));
 		System.out.println();
@@ -15,6 +16,12 @@ public class EvalucionGit {
 		System.out.println("El pequeño cocodrilo\npara aprender sus cantares");
 		System.out.println("usa las aguas del Nilo\ncon sus notas musicales");
 		
+		for(int i=b; i>0; i--)
+		{
+			factorial=factorial*i;
+		}
+		
+		System.out.println("\nEl factorial de "+b+" es "+factorial);
 	}
 
 }
